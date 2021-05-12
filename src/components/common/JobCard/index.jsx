4537c52877dfa716018285e0ae8175b07d939cc5
Cesar Luis Rijo <cesarluisrc@gmail.com>
2021-05-12 08:57:28 -0400

    Changes

--- src/components/common/JobCard/index.jsx
+++ src/components/common/JobCard/index.jsx
@@ -1,24 +1,22 @@
 import React from "react";
 import JobType from "../JobType";
 import "./styles.scss";
-import image from "../../../assets/search-bg.jpeg";
 import IconInfo from "../IconInfo";
 
 const JobCard = (props) => {
   return (
     <div className="job-card">
       <div className="job-card__main">
-        <img src={image} alt="hola" />
+        <img src={props.image} alt={props.jobName} />
         <div className="job-card__info">
-          <div className="bussines-name">Kasisto</div>
-          <div className="job-name">Front-End Software Engineer</div>
-          <JobType type="Full time" />
-          {/* {props.type && <JobType type="Full time" />} */}
+          <div className="bussines-name">{props.bussines}</div>
+          <div className="job-name">{props.jobName}</div>
+          {props.type && <JobType type={props.type} />}
         </div>
       </div>
       <div className="icons-zone">
-        <IconInfo icon="public" text="New York" />
-        <IconInfo icon="schedule" text="5 day ago" />
+        <IconInfo icon="public" text={props.location} />
+        <IconInfo icon="schedule" text={props.date} />
       </div>
     </div>
   );
